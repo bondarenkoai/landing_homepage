f4c617694cceb85dccaa2df6bd3c5081892eddc3
bondarenkoai <114067419+bondarenkoai@users.noreply.github.com>
2024-01-26 16:15:13 +0000

    Deploying to gh-pages from @ bondarenkoai/landing_homepage@0c5b34d1ec8ed130068aa17fdc84dd93b8867a24 🚀

---
+++ index.e374e2a5.js
@@ -0,0 +1,2 @@
+var navLinks=document.querySelectorAll(".site-nav__link");navLinks.forEach((function(n){n.addEventListener("click",(function(){navLinks.forEach((function(n){return n.classList.remove("active")})),n.classList.add("active")}))}));
+//# sourceMappingURL=index.e374e2a5.js.map
