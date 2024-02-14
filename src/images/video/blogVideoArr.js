34d06ab92943309faf72c5b8735509d9b02ce23c
bondarenkoai <114067419+bondarenkoai@users.noreply.github.com>
2024-02-15 00:02:42 +0200

    video edit

---
+++ src/images/video/blogVideoArr.js
@@ -0,0 +1,41 @@
+const blogVideoArr = [
+    {
+        id: 1,
+        src: URL('https://www.youtube.com/embed/1j3uqlGrh4E'),
+        alt: 'Video load error. Sorry!',
+        description: 'Perfectible Hills ',
+        place: 'Kyiv, Ukraine',
+    },
+    {
+        id: 1,
+        src: URL('https://www.youtube.com/embed/1j3uqlGrh4E'),
+        alt: 'Video load error. Sorry!',
+        description: 'Perfectible Hills ',
+        place: 'Kyiv, Ukraine',
+    },
+    {
+        id: 3,
+        src: URL('https://vimeo.com/379544045'),
+        alt: 'Video load error. Sorry!',
+        description: ' Hills ',
+        place: 'Carpathian, Ukraine',
+    },
+    {
+        id: 4,
+        src: URL(
+            'https://cdn.coverr.co/videos/coverr-opera-house-in-lviv-6497/1080p.mp4'
+        ),
+        alt: 'Video load error. Sorry!',
+        description: 'Opera House in Lviv',
+        place: 'Lviv, Ukraine',
+    },
+    {
+        id: 5,
+        src: URL(
+            'https://cdn.coverr.co/videos/coverr-odessa-beach-7293/1080p.mp4'
+        ),
+        alt: 'Video load error. Sorry!',
+        description: 'Beautiful Odessa Beach',
+        place: 'Odesa, Ukraine',
+    },
+];
